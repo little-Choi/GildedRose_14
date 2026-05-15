0a2c193afa241bc3a330d820a7877d3a5cee7460
little-Choi <dkssudsdkssu@gmail.com>
2026-05-15 10:15:26 +0900

    조건 정리 1

--- cpp/src/GildedRose.cpp
+++ cpp/src/GildedRose.cpp
@@ -5,50 +5,48 @@ GildedRose::GildedRose(std::vector<Item> &items) : items(items) {}
 void GildedRose::updateQuality() {
   for (size_t i = 0; i < items.size(); i++) {
     Item &item = items[i];
-    if (item.name != AGED_BRIE && item.name != BACKSTAGE_PASS) {
-      if (item.quality > MIN_QUALITY) {
-        if (item.name != SULFURAS) {
-          item.quality = item.quality - 1;
-        }
-      }
-    } else {
+    if (item.name == AGED_BRIE || item.name == BACKSTAGE_PASS) {
       if (item.quality < MAX_QUALITY) {
-        item.quality = item.quality + 1;
+        item.quality++;
 
         if (item.name == BACKSTAGE_PASS) {
           if (item.sellIn < 11) {
             if (item.quality < MAX_QUALITY) {
-              item.quality = item.quality + 1;
+              item.quality++;
             }
           }
-
           if (item.sellIn < 6) {
             if (item.quality < MAX_QUALITY) {
-              item.quality = item.quality + 1;
+              item.quality++;
             }
           }
         }
       }
+    } else if (item.name == SULFURAS) {
+    } else {
+      if (item.quality > MIN_QUALITY) {
+        item.quality--;
+      }
     }
 
     if (item.name != SULFURAS) {
-      item.sellIn = item.sellIn - 1;
+      item.sellIn--;
     }
 
     if (item.sellIn < 0) {
-      if (item.name != AGED_BRIE) {
+      if (item.name == AGED_BRIE) {
+        if (item.quality < MAX_QUALITY) {
+          item.quality++;
+        }
+      } else {
         if (item.name != BACKSTAGE_PASS) {
           if (item.quality > MIN_QUALITY) {
             if (item.name != SULFURAS) {
-              item.quality = item.quality - 1;
+              item.quality--;
             }
           }
         } else {
-          item.quality = item.quality - item.quality;
-        }
-      } else {
-        if (item.quality < MAX_QUALITY) {
-          item.quality = item.quality + 1;
+          item.quality = MIN_QUALITY;
         }
       }
     }
